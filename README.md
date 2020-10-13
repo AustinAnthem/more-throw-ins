28c5a8a1fa0d4e6e560e8fea5342cee19dd8620d
Tony Cardone <tony.cardone@gmail.com>
2020-10-13 17:38:30 -0500

    Disclaimer adjustment
    Tweaking disclaimer by request and adding readme for production release.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# more-throw-ins
-Austin Soccer supporting Prop A and Prop B
+# Austin Soccer Votes
+The AustinSoccerVotes.org website is a collaborative effort between [Austin Anthem](https://austinanthem.org) and [Los Verdes](https://losverdesatx.org) encouraging all Austin soccer supporters to vote for two very important propositions in the 2020 election. Fulfilling our commitment to our city council members to stay involved in local matters for the better.
+
+# Technical Details
+This site is powered by [Vue](https://vuejs.org/) and hosted on Microsoft Azure's Static Web Apps (Beta). Though it's mostly tables and basic HTML due to the timeline of the project.
+
+## Quickstart
+No fancy modifications here. Make sure you have [npm](https://www.npmjs.com/) installed, then, from the root directory of the project:
+`npm install` installs all the node dependencies
+`npm run serve` starts a development server and tells you where to hit it locally for development.
+
+If you want a production build for some reason, then `npm run-script build` has you covered. In this case, GitHub Actions do all the magic to push it into Azure with literally no effort.
--- src/components/Disclaimer.vue
+++ src/components/Disclaimer.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="disclaimer">
-        Austin Soccer Votes is a collaborative effort between <a href="https://austinanthem.org"> Austin Anthem</a> and <a href="https://losverdesatx.org">Los Verdes</a>, the independent supporters groups of Austin FC. Neither organization offers endorsements of candidates, and external content may not reflect the views of the organizations.| <a href="mailto:digital@austinanthem.org">Powered by Anthem Digital</a>. Help us build the technology behind Austin soccer. | <a href="https://github.com/AustinAnthem">GitHub</a>
+        Austin Soccer Votes is a collaborative effort between <a href="https://austinanthem.org"> Austin Anthem</a> and <a href="https://losverdesatx.org">Los Verdes</a>, the independent supporters groups of Austin FC. Neither organization offers endorsements of candidates, and external content may not reflect the views of the organizations.| <a href="mailto:digital@austinanthem.org">Hosted by Anthem Digital</a>. | <a href="https://github.com/AustinAnthem/more-throw-ins">GitHub</a>
     </div>
 </template>
 <style scoped>
 #disclaimer{
     color: darkslategray;
-    font-style: italic;
     font-size: 14px;
+    width: 80%;
+    padding-right: 10%;
+    padding-left: 10%
 }
 </style>
